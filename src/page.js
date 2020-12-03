9bbcad8ae486c2e9c0f78452f9cd2da773e85c38
Srinivasalu109 <71975393+Srinivasalu109@users.noreply.github.com>
2020-12-03 13:08:37 +0530

    Add files via upload

---
+++ src/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./school.css";
+export default function Page1(){
+    return(<div id="page1">
+        <h1>Choose Your Catogory</h1>
+       <button>student</button>
+       <button>school-admin</button>
+    </div>)
+
+} 
